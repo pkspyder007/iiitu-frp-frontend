1eddda410bb1bbc68b1a76bfe81544dca42b65e0
Praveen Kumar <pkspyder007@gmail.com>
2021-03-03 15:02:42 +0530

    Updated Other info

--- src/pages/Dashboard/OtherInfo.js
+++ src/pages/Dashboard/OtherInfo.js
@@ -6,12 +6,12 @@ import SecondaryInput from "../../components/SecondaryInput";
 import AppLayout from "./AppLayout";
 
 const initState = {
-  awards: "",
-  extraCirricular: "",
-  membership: "",
-  special: "",
-  others: "",
-  appId: "",
+  type: "",
+  by: "",
+  title: "",
+  date: "",
+  amount: "",
+  doc: "",
 };
 
 export default function OtherInfo() {
@@ -33,7 +33,6 @@ export default function OtherInfo() {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
-   
   const onSubmit = (e) => {
     e.preventDefault();
     let data = new FormData();
@@ -55,91 +54,119 @@ export default function OtherInfo() {
       });
   };
 
-
   return (
     <AppLayout>
-    <form id="sopform" onSubmit={onSubmit}>
-      <div className="editor w-screen mb-10 w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
-      <h1 className="text-2xl text-indigo-600 mb-4">Other Information </h1>
-        <label htmlFor="awards" className="text-sm mb-1">
-          Awards, Honors, Academic Credentials and Recognitions <br /> ( Attach
-          pdf containing required details )
-        </label>
-        <SecondaryInput
-          onChange={onFileChangeHandler}
-          id="awards"
-          name="awards"
-          type="file"
-        />
+      <form id="sopform" onSubmit={onSubmit}>
+        <div className="editor w-screen mb-10 w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
+          <h1 className="text-2xl text-indigo-600 mb-4">Other Information </h1>
 
-        <label htmlFor="extraCirricular" className="text-sm mb-1">
-          Extra-curricular activities <br />( Attach pdf containing required
-          details )
-        </label>
-        <SecondaryInput
-          onChange={onFileChangeHandler}
-          id="extraCirricular"
-          name="extraCirricular"
-          type="file"
-        />
-
-        <hr className="mb-4" />
-
-        <label htmlFor="membership" className="text-sm mb-1">
-        Memberships, Fellowships of Professional Societies
-          
-        </label>
-        <textarea
-          onChange={onChangeHandler}
-          id="membership"
-          name="membership"
-          type="file"
-          value={state.membership}
-          className="description bg-gray-100 sec p-3 mb-4 h-40 border border-gray-300 outline-none"
-        ></textarea>
-        <label htmlFor="special" className="text-sm mb-1">
-        Special training, Proficiency or Expertise and Relevant Information
-          
-        </label>
-        <textarea
-          onChange={onChangeHandler}
-          id="special"
-          name="special"
-          type="file"
-          value={state.special}
-          className="description bg-gray-100 sec p-3 mb-4 h-40 border border-gray-300 outline-none"
-        ></textarea>
-        <label htmlFor="others" className="text-sm mb-1">
-        Any other information not stated before
-          
-        </label>
-        <textarea
-          onChange={onChangeHandler}
-          id="others"
-          name="others"
-          type="file"
-          value={state.others}
-          className="description bg-gray-100 sec p-3 mb-4 h-40 border border-gray-300 outline-none"
-        ></textarea>
-
-        <div className="buttons flex mt-8">
-          <div
-            onClick={resetForm}
-            className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto"
+          <label htmlFor="eduMode" className="text-sm mb-1">
+            Select Entity Type Mode <span className="text-red-500">*</span>
+          </label>
+          <select
+            onChange={onChangeHandler}
+            className="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none"
+            value={state.type}
+            id={"eduMode"}
+            name={"type"}
+            required={true}
+            placeholder=" Select Entity type "
           >
-            Reset
-          </div>
-          <div className="">
-            <button
-              className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-white ml-2 bg-indigo-600"
-              type={"submit"}
+            <option value="">Select Type</option>
+            <option value="normal">Award</option>
+            <option value="dual">Honor</option>
+            <option value="dphd">Academic Credentials/Recognization</option>
+            <option value="dphd">
+              Membership/ Fellowship of an Professional society
+            </option>
+            <option value="dphd">Special Training</option>
+            <option value="dphd">Proficiency / Revent Information</option>
+            <option value="dphd">Others</option>
+          </select>
+
+          <label htmlFor="subject" className="text-sm mb-1">
+            Title <span className="text-red-500">*</span>
+          </label>
+          <SecondaryInput
+            onChange={onChangeHandler}
+            id="subject"
+            name="title"
+            type="text"
+            value={state.title}
+            required={true}
+            placeholder={"title"}
+          />
+
+          <hr className="mb-4" />
+          <label htmlFor="subject" className="text-sm mb-1">
+            Awarding Organization <span className="text-red-500">*</span>
+          </label>
+          <SecondaryInput
+            onChange={onChangeHandler}
+            id="subject"
+            name="by"
+            type="text"
+            value={state.by}
+            required={true}
+            placeholder={"Awarding Organization"}
+          />
+
+          <label htmlFor="subject" className="text-sm mb-1">
+            Amount <span className="text-red-500">( If Any )</span>
+          </label>
+          <SecondaryInput
+            onChange={onChangeHandler}
+            id="subject"
+            name="amount"
+            type="number"
+            value={state.amount}
+            required={true}
+            placeholder={"Amount if any"}
+          />
+          <label htmlFor="subject" className="text-sm mb-1">
+            Date of Award/membership/honour{" "}
+            <span className="text-red-500">*</span>
+          </label>
+          <SecondaryInput
+            onChange={onChangeHandler}
+            id="subject"
+            name="date"
+            type="date"
+            value={state.date}
+            required={true}
+            placeholder={"Date of Award/membership/honour"}
+          />
+
+          <label htmlFor="awards" className="text-sm mb-1">
+            Upload relevant certificate/document.
+          </label>
+          <SecondaryInput
+            onChange={onFileChangeHandler}
+            id="awards"
+            name="doc"
+            type="file"
+          />
+
+          <hr className="mb-4" />
+
+          <div className="buttons flex mt-8">
+            <div
+              onClick={resetForm}
+              className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto"
             >
-              Add
-            </button>
+              Reset
+            </div>
+            <div className="">
+              <button
+                className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-white ml-2 bg-indigo-600"
+                type={"submit"}
+              >
+                Add
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
     </AppLayout>
   );
 }
